1a16b7ae762b368b7d361785c04fea35e285621e
Patricia <paty.silveira.@hotmail.com>
2025-08-14 14:42:03 -0300

    Aula 12 - Arrays

---
+++ Aula12/ArrayExerc.js
@@ -0,0 +1,15 @@
+// Arrays are a way to store multiple values in a single variable.
+// They can hold any type of data, including numbers, strings, and even other arrays.
+
+let thingsILike = ["Pizza", "Movies", "Music", "Chocolate", "Dance"];
+    console.log(thingsILike.pop())
+    console.log(thingsILike.push())
+    console.log(thingsILike.unshift())
+    console.log(thingsILike.shift())
+// There are many methods to manipulate arrays.
+// Access w3schools for more information: https://www.w3schools.com/js/js_array_methods.asp
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log( myChildren[0])
